fce06b11815670dbd78b9df672c1a06792aa1a59
avidit <aabishkar@gmail.com>
2016-10-30 22:52:43 -0400

    289 Easy

--- 289_easy_its_super_effective.py
+++ 289_easy_its_super_effective.py
@@ -1,3 +1,44 @@
+"""
+https://redd.it/5961a5
+
+Description
+
+In the popular Pokemon games all moves and Pokemons have types that determine 
+how effective certain moves are against certain Pokemons. These work by some 
+very simple rules, a certain type can be super effective, normal, not very 
+effective or have no effect at all against another type. These translate 
+respectively to 2x, 1x, 0.5x and 0x damage multiplication. If a Pokemon has 
+multiple types the effectiveness of a move against this Pokemon will be the 
+product of the effectiveness of the move to it's types.
+
+Formal Inputs & Outputs
+
+Input
+
+The program should take the type of a move being used and the types of the 
+Pokemon it is being used on.
+
+Example inputs
+ fire -> grass
+ fighting -> ice rock
+ psychic -> poison dark
+ water -> normal
+ fire -> rock
+Output
+
+The program should output the damage multiplier these types lead to.
+
+Example outputs
+2x
+4x
+0x
+1x
+0.5x
+
+Use the Pokemon api to calculate the output damage.
+Like, http://pokeapi.co/api/v2/type/fire/
+"""
+
 import requests
 from operator import mul
 from functools import reduce
@@ -30,9 +71,9 @@ def parse_damage_relations(attacker, defenders, data):
     for damage_rel, multiplier in DAMAGES.items():
         damage_types = damage_relations[damage_rel]
         
-    for damage_type in damage_types:
-        if damage_type['name'] in defenders:
-            output[damage_type['name']] = multiplier
+        for damage_type in damage_types:
+            if damage_type['name'] in defenders:
+                output[damage_type['name']] = multiplier
 
     return output
 

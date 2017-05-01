cf9704bd4c8a57faa25fc02fcd9420cb32e744ad
avidit <aabishkar@gmail.com>
2017-04-30 20:44:02 -0400

    293 easy

---
+++ 293/easy/293_easy_defusing_the_bomb.py
@@ -0,0 +1,22 @@
+rules = {
+    'white': [False, ['white', 'black']],
+    'red': [True, ['green']],
+    'black': [False, ['white', 'green', 'orange']],
+    'orange': [True, ['red', 'black']],
+    'green': [True, ['orange', 'white']],
+    'purple': [False, ['purple', 'green', 'orange', 'white']]
+}
+
+
+def defuse_bomb(wires):
+    state = 'Defused'
+    for i, wire in enumerate(wires[:-1]):
+        if rules[wire][0] != (wires[i + 1] in rules[wire][1]):
+            state = 'Boom'
+    return state
+
+
+if __name__ == "__main__":
+    print(defuse_bomb(['white', 'red', 'green', 'white']))
+    print(defuse_bomb(['white', 'orange', 'green', 'white']))
+
